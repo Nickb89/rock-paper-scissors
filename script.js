d973bf5c5061085318a07650db94574d61970415
Nickb89 <nickb8989@gmail.com>
2024-01-12 22:06:23 +0000

    Title: Add UI elements with styles Add replay button to reset game Add images for buttons Hide buttons based on if game is over keep track of game rounds played use flexbox to arrange elements

--- script.js
+++ script.js
@@ -3,37 +3,45 @@ let cpuScore = 0;
 let gameRounds = 0;
 let playerChoice = "";
 
+
+
+
+
+//TO DO
+
+//on mouse enter javascript event listner to enlarge image
+//replay button resets game
+//not happy with look
+
+
+
+
 //Get element references and create button div.
 const mainDiv = document.querySelector('#GameArea');
 const buttonsDiv = document.createElement('div');
 buttonsDiv.setAttribute('id', 'buttonsDiv');
 
-//add Player and CPU Score display
-const playerScoreText = document.createElement('p');
-playerScoreText.textContent = "Player Score: " + playerScore.toString();
-
-const CPUScoreText = document.createElement('p');
-CPUScoreText.textContent = "Computer Score: " + cpuScore.toString();
 
 
-//add textArea for displaying initial text and outcome
-const textDiv = document.createElement('div');
-textDiv.setAttribute("class", "textArea");
-textDiv.textContent = "Rock Paper or Scissors?";
-mainDiv.appendChild(textDiv);
 
-mainDiv.appendChild(playerScoreText);
-mainDiv.appendChild(CPUScoreText);
-
-//add 3 buttons
+//add 3 buttons for player choice
 const btnRock = document.createElement('button');
-btnRock.textContent = "Rock";
+btnRock.style.backgroundImage = "url('images/rock.png')";
+btnRock.style.backgroundSize = "100px 100px";
+btnRock.style.backgroundRepeat = "no-repeat";
+btnRock.style.backgroundPosition = "center";
 
 const btnPaper = document.createElement('button');
-btnPaper.textContent = "Paper";
+btnPaper.style.backgroundImage = "url('images/paper.png')";
+btnPaper.style.backgroundSize = "100px 100px";
+btnPaper.style.backgroundRepeat = "no-repeat";
+btnPaper.style.backgroundPosition = "center";
 
 const btnScissors = document.createElement('button');
-btnScissors.textContent = "Scissors";
+btnScissors.style.backgroundImage = "url('images/scissors.png')";
+btnScissors.style.backgroundSize = "100px 100px";
+btnScissors.style.backgroundRepeat = "no-repeat";
+btnScissors.style.backgroundPosition = "center";
 
 
 buttonsDiv.appendChild(btnRock);
@@ -42,10 +50,54 @@ buttonsDiv.appendChild(btnScissors);
 
 mainDiv.appendChild(buttonsDiv);
 
+//add Player and CPU Score display, current round.
+const scoreDiv = document.createElement('div');
+scoreDiv.setAttribute("class", "scoreDiv");
+
+const playerScoreText = document.createElement('p');
+playerScoreText.textContent = "Player Score: " + playerScore.toString();
+
+const CPUScoreText = document.createElement('p');
+CPUScoreText.textContent = "Computer Score: " + cpuScore.toString();
+
+const currentRoundText = document.createElement('p');
+currentRoundText.textContent = "Round: " + gameRounds;
+
+scoreDiv.appendChild(playerScoreText);
+scoreDiv.appendChild(currentRoundText);
+scoreDiv.appendChild(CPUScoreText);
+
+
+//add textArea for displaying round results
+
+const textDiv = document.createElement('div');
+textDiv.setAttribute("class", "textArea");
+textDiv.textContent = "Rock Paper or Scissors?";
+
+mainDiv.appendChild(textDiv);
+mainDiv.appendChild(scoreDiv);
+
 //add restart button
 const replayBtn = document.createElement('button');
-replayBtn.setAttribute('hidden', 'true');
-replayBtn.textContent = "Replay 5 Rounds?";
+replayBtn.setAttribute('class', 'hideItem');
+replayBtn.textContent = "Replay";
+
+replayBtn.addEventListener('click', function(){
+    gameRounds = 0;
+    playerScore = 0;
+    cpuScore = 0;
+    playerScoreText.textContent = "Player Score: " + playerScore;
+    CPUScoreText.textContent = "Computer Score: " + cpuScore;
+    currentRoundText. textContent = "Round: " + gameRounds;
+    textDiv.textContent = "Rock Paper or Scissors?";
+    btnRock.classList.toggle('hideItem');
+    btnPaper.classList.toggle('hideItem');
+    btnScissors.classList.toggle('hideItem');
+    this.setAttribute('class', 'hideItem');
+
+
+})
+
 buttonsDiv.appendChild(replayBtn);
 
 
@@ -63,6 +115,7 @@ function getComputerChoice()  {
         if(gameRounds <=4 ){
         playerChoice = "rock";
         playRound(playerChoice, getComputerChoice());
+        currentRoundText.textContent = "Round: " + gameRounds;
         if (gameRounds == 5){
             GameOver();
         };
@@ -74,6 +127,7 @@ function getComputerChoice()  {
         if(gameRounds <=4 ){
             playerChoice = "scissors";
             playRound(playerChoice, getComputerChoice());
+            currentRoundText.textContent = "Round: " + gameRounds;
             if (gameRounds == 5){
                 GameOver();
             };
@@ -85,6 +139,7 @@ function getComputerChoice()  {
         if(gameRounds <=4 ){
             playerChoice = "paper";
             playRound(playerChoice, getComputerChoice());
+            currentRoundText.textContent = "Round: " + gameRounds;
             if (gameRounds == 5){
                 GameOver();
             };
@@ -143,29 +198,41 @@ function playRound(playerSelection, compSelection) {
 
 };
 
+function checkIfFinalScoreAboveOne(){
+    if (playerScore > 1){
+        return " points";
+    } else if (playerScore <=1){
+        return " point";
+    }
+}
+
     function GameOver() {
 
         if(playerScore > cpuScore){
-            textDiv.textContent = 'You won with ' + playerScore + " points. Computer had "
+            textDiv.textContent = 'You won with ' + playerScore + checkIfFinalScoreAboveOne() + ". Computer had "
             + cpuScore + " points" ;
             btnRock.setAttribute("hidden", "false");
             btnPaper.setAttribute("hidden", "false");
             btnScissors.setAttribute("hidden", "false");
         } else if (playerScore < cpuScore) {
-            textDiv.textContent = 'You lost with ' + playerScore + " points. Computer had "
+            textDiv.textContent = 'You lost with ' + playerScore + checkIfFinalScoreAboveOne() + ". Computer had "
             + cpuScore + " points" ;
             btnRock.setAttribute("hidden", "false");
             btnPaper.setAttribute("hidden", "false");
             btnScissors.setAttribute("hidden", "false");
             
         } else if(playerScore == cpuScore){
-            textDiv.textContent = 'you TIED with ' + playerScore + " points.";
+            textDiv.textContent = 'you TIED with ' + playerScore + checkIfFinalScoreAboveOne() + ".";
             btnRock.setAttribute("hidden", "false");
             btnPaper.setAttribute("hidden", "false");
             btnScissors.setAttribute("hidden", "false");
         }
        
-        replayBtn.removeAttribute("hidden", "true");
+        replayBtn.setAttribute('class', 'replayBtn');
+        btnRock.classList.toggle('hideItem');
+        btnPaper.classList.toggle('hideItem');
+        btnScissors.classList.toggle('hideItem');
+        
         
     };
 
